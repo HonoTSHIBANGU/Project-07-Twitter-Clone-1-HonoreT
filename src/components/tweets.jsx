ea2ab507a80821f2bba4d1ad65af6ae1b6f37e08
Tshibangu_honore <honor.tshibangu@gmail.com>
2023-11-23 15:17:38 +0100

    clone_twitter_2

--- src/components/tweets.jsx
+++ src/components/tweets.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import photo from '../images/tweet-profile-photo.png';
+
+
 
 function Tweets() {
     return (
-        <div className='tweet'></div>
+        <div className='tweet'>
+            <img src={photo} alt='' />
+            <p>
+                <span>CNN</span>
+            </p>
+            <p>
+                President Joe Biden touted a new agreement reached with the european union to ease Trump-era
+                tariffs on aluminuim and stell as a "major breakthrough" that would serve to both strengthen the
+                US steel industry and combat the global climate crisis.
+            </p>
+
+            <div className='tweet-content'>
+                <div className='tweet-body'>
+                    <p className='tweet-title'>The new york Times</p>
+                </div>
+
+            </div>
+        </div>
+        
+        <div></div>
+
+
+
+
     )
 }
+
 
 export default Tweets
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,12 +2,14 @@
 import React from 'react'
 import Header from '../components/header';
 import TweetEditor from '../components/TweetEditor';
+import Tweets from '../components/tweets';
 
 function home() {
   return (
     <main className='timeline'>
       <Header />
       <TweetEditor />
+      <Tweets />
 
     </main>
   )
