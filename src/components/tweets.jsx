61ee2314f919913aca4a057d0267f3071963fa00
Tshibangu_honore <honor.tshibangu@gmail.com>
2023-11-24 10:35:19 +0100

    clone_twitter4

--- src/components/tweets.jsx
+++ src/components/tweets.jsx
@@ -1,38 +1,40 @@
 import React from 'react'
 import photo from '../images/tweet-profile-photo.png';
 import photo4 from '../images/verified.svg';
-import Reply1 from '../images/Tweet-Replies1.svg';
-import Reply2 from '../images/Tweet-Replies2.svg';
-import Reply3 from '../images/Tweet-Replies3.svg';
-import Reply4 from '../images/Tweet-Replies4.svg';
+import photo5 from '../images/a1Tweet-Replies.svg';
+import photo6 from '../images/a2Tweet-Replies.svg';
+import photo7 from '../images/a3Tweet-Replies.svg';
+import photo8 from '../images/a4Tweet-Replies.svg';
 
 
 
 function Tweets() {
     return (
         <div className='tweets'>
+
             <div className='tweet'>
                 <img src={photo} alt='' />
-                <p>
+                <p >
                     <span>CNN</span>
                     <span><img src={photo4} alt="" /></span>
                     <span className='tweet-title-details'>@CNN</span>
                 </p>
-                <p className=''>
+                <p className='tweet-text'>
                     President Joe Biden touted a new agreement reached with the european union to ease Trump-era
                     tariffs on aluminuim and stell as a "major breakthrough" that would serve to both strengthen the
                     US steel industry and combat the global climate crisis.
+
                 </p>
-
-                <div className='tweet-actions'>
-                    <div className='tweet-action'><img src={Reply1} alt='' /></div>
-                    <div className='tweet-action'><img src={Reply2} alt='' /></div>
-                    <div className='tweet-action'><img src={Reply3} alt='' /></div>
-                    <div className='tweet-action'><img src={Reply4} alt='' /></div>
+                <div className='tweet'>
+                    <div ><img src={photo5} alt="" /></div>
+                    <div ><img src={photo6} alt="" /></div>
+                    <div ><img src={photo7} alt="" /></div>
+                    <div ><img src={photo8} alt="" /></div>
                 </div>
-
-
             </div>
+
+
+
 
         </div>
 
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -15,6 +15,7 @@ function home() {
       <TweetContent />
 
 
+
     </main>
   )
 }
