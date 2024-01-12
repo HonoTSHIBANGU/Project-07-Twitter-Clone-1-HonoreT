7de66715cd3b9cdc8f8042717bb0cc53f8f8aa6d
HonoTSHIBANGU <honor.tshibangu@gmail.com>
2024-01-12 15:05:45 +0100

    correction de la partie userName

--- src/pages/UserName.jsx
+++ src/pages/UserName.jsx
@@ -3,9 +3,14 @@ import Photo from "../images/hh.jpg";
 
 function Username() {
   return (
-    <div>
-      <div className="username">
-        <img src={Photo} alt="" />
+    <div className="border border-l-1 border-r-1 border-[#2F3336] px-60 ">
+      <div className="">
+        <img
+          className="w-60 h-60 rounded-full relative right-6 top-40"
+          src={Photo}
+          alt=""
+        />
+        <h3 className="relative right-6 top-60">TSHIBANGU Honoré</h3>
       </div>
     </div>
   );
