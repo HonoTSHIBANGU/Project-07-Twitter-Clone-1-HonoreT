70ab83869fb1f726b399987b7f0c7616ff71606e
HonoTSHIBANGU <honor.tshibangu@gmail.com>
2024-01-18 00:57:28 +0100

    users2

--- src/model/user.js
+++ src/model/user.js
@@ -5,6 +5,8 @@ import Photo11 from "../images/Tweet-Profile-Photo.svg";
 import Photo13 from "../images/hh.jpg";
 import Photo14 from "../images/Profile-Photo3.png";
 import coverPhotoCNN from "../images/photoDecouvertureCNN.jpg";
+import coverPhotoTimes from "../images/newYorkTimes.jpg";
+
 
  const users = [
     {id:0,
@@ -18,7 +20,7 @@ import coverPhotoCNN from "../images/photoDecouvertureCNN.jpg";
       dateOfInscription: " A rejoint Twitter en février 2007",
       numberOfSubscriptions: "1086 abonnements",
       NumberOfSubscribers: "62 M abonnés",
-      followedBy: "suivi par Theodore et Ghyslain",
+      followedBy: "suivi par .............",
       coverPhoto: coverPhotoCNN,
       tweetText:
         'President Joe Biden touted a new agreement reached with the European Union to ease Trump-era tariffs on aluminum and steel as a "major breakthrough" that would serve to both strengthen the US steel industry and combat the global climate crisis.',
@@ -29,6 +31,13 @@ import coverPhotoCNN from "../images/photoDecouvertureCNN.jpg";
       imageSrc: Photo11,
       Image: Photo12,
       authorAvatarSrc: Photo10,
+      description: "it's our job #Gothere & tell the most difficult stories, For breaking news, follow @CNNBRK and download our app cnn.com/apps ",
+      languageTradiction: "Traduire la biographie",
+      dateOfInscription: " A rejoint Twitter en février 2007",
+      numberOfSubscriptions: "1086 abonnements",
+      NumberOfSubscribers: "62 M abonnés",
+      followedBy: "suivi par .............",
+      coverPhoto: coverPhotoTimes,
       tweetText:
         "Gardening boomed during the pandemic. Six Black writers share how it has helped them re-establish, and reimagine, a connection to cultivation and the land.",
     },
--- src/pages/userNameAll.jsx
+++ src/pages/userNameAll.jsx
@@ -11,12 +11,41 @@ function UserNameAll() {
   });
 
   return (
-    <div>
-      <img src={oneData.coverPhoto} alt="" />
-      <img src={oneData.imageSrc} alt="" />
-      <img src={oneData.authorAvatarSrc} alt="" />
-      <h1>{oneData.autor}</h1>
-      <p>{oneData.autorDetail} </p>
+    <div className="border-b border-[1px] border-solid border-[#2F3336] ml-20 mr-8 flex-1 flex-shrink-0 basis-800">
+      <div className="relative top-10">
+        <img src={oneData.coverPhoto} alt="" />
+      </div>
+      <div className="relative bottom-10 left-6 w-40 h-20">
+        <img src={oneData.imageSrc} alt="" />
+      </div>
+      <div className="flex gap-3 relative top-14 left-3">
+        <h1 className="text-lg font-bold">{oneData.autor}</h1>
+
+        <img
+          className="w-5 h-5 relative top-1 "
+          src={oneData.authorAvatarSrc}
+          alt=""
+        />
+      </div>
+      <div>
+        <p className="relative top-14 left-3 text-gray-500 ">
+          {oneData.autorDetail}
+        </p>
+      </div>
+      <div className="relative top-16 left-3">
+        <p>{oneData.description}</p>
+      </div>
+      <div className="relative top-16 left-3 text-blue-500">
+        <button>{oneData.languageTradiction} </button>
+      </div>
+      <div className="relative top-20 left-3">
+        <p>{oneData.dateOfInscription} </p>
+      </div>
+      <div className="flex gap-2 relative top-24 left-3">
+        <div>{oneData.numberOfSubscriptions}</div>
+        <div>{oneData.NumberOfSubscribers}</div>
+      </div>
+      <div className="relative top-24 left-3">{oneData.followedBy} </div>
     </div>
   );
 }
