c9aca5f9266cd954f0823d060c378a804a51569a
HonoTSHIBANGU <honor.tshibangu@gmail.com>
2024-01-24 13:51:57 +0100

    ajout nouveau tweet2

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
 import Layout from "./components/layout.jsx";
 import Home from "./pages/Home.jsx";
 import Tweet from "./model/Tweet.js";
-import TweetProvider from "./contexte/TweetProvider.jsx";
 
 import("./style/reset.css");
 import("./style/App.css");
 
 export default function App() {
   return (
-    <TweetProvider>
-      <Layout>
-        <Home />
-      </Layout>
-    </TweetProvider>
+    <Layout>
+      <Home />
+    </Layout>
   );
 }
